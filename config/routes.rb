c5f33e549c6d49f0ccacbf229412d1c4fded0f3e
VILLAMOR GO <villamorgo@gmail.com>
2022-03-17 00:22:02 +0800

    Configured routes and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   get 'stock/index'
-  get '/users/request/', to: 'users#show_request', as: 'users/request'
   root 'home#index'
   devise_for :users, :path_prefix => 'devise'
-  resources :users, only: [:index, :show, :edit, :update, :create, :new, :request]
+  resources :users, only: [:index, :show, :edit, :update, :create, :new]
 
-  resources :users do
-    member do
-      patch :update_state
-    end
-  end
   resources :stocks
+  resources :requests, only: [:index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
