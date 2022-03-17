45b9a36dd10e27600a24f1eaf10ae8378f1fbcbf
VILLAMOR GO <villamorgo@gmail.com>
2022-03-17 22:43:26 +0800

    modify routes for pending requests

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,9 @@ Rails.application.routes.draw do
   get '/users/request/', to: 'users#show_request', as: 'users/request'
   root 'home#index'
   devise_for :users, :path_prefix => 'devise'
-  resources :users, only: [:index, :show, :edit, :update, :create, :new, :request]
+  resources :users, only: [:index, :show, :edit, :update, :create, :new]
 
-  resources :users do
-    member do
-      patch :update_state
-    end
-  end
   resources :stocks
+  resources :pending_requests, only: [:index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
