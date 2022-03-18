ff56567738d1dcbd7a712c45ace14b9415e6bf82
nicopleyto <nicopleyto@gmail.com>
2022-03-18 19:08:22 +0800

    added access quote method to portfolio_stock and transaction models. needs refactoring

--- app/models/portfolio_stock.rb
+++ app/models/portfolio_stock.rb
@@ -1,3 +1,15 @@
 class PortfolioStock < ApplicationRecord
     belongs_to :user
+
+    def access_quote
+        initialize_client
+        @client.quote(self.symbol)
+    end
+
+    private
+
+    def initialize_client
+        #token credentials can be found in config/initializers/iex_client.rb
+        @client ||= IEX::Api::Client.new
+    end
 end
