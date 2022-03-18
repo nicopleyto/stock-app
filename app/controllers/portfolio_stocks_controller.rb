19a1824b69a62bf006a852bcb92e55443012709c
nicopleyto <nicopleyto@gmail.com>
2022-03-18 18:25:47 +0800

    added show action to portfolio stocks controller

--- app/controllers/portfolio_stocks_controller.rb
+++ app/controllers/portfolio_stocks_controller.rb
@@ -2,4 +2,8 @@ class PortfolioStocksController < ApplicationController
   def index
     @portfolio_stocks = current_user.portfolio_stocks
   end
+
+  def show
+    @portfolio_stock = current_user.portfolio_stocks.find(params[:id])
+  end
 end
