17aa1e3f1691690a8d4683b579d07c099e9a143a
nicopleyto <nicopleyto@gmail.com>
2022-03-19 11:17:55 +0800

    added api accessor module to declutter transaction, stock, and portfolio_stock models

--- app/models/portfolio_stock.rb
+++ app/models/portfolio_stock.rb
@@ -1,15 +1,6 @@
 class PortfolioStock < ApplicationRecord
     belongs_to :user
 
-    def access_quote
-        initialize_client
-        @client.quote(self.symbol)
-    end
-
-    private
-
-    def initialize_client
-        #token credentials can be found in config/initializers/iex_client.rb
-        @client ||= IEX::Api::Client.new
+    include ApiAccessor
     end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,15 +1,5 @@
 class Transaction < ApplicationRecord
     belongs_to :user
 
-    def access_quote
-        initialize_client
-        @client.quote(self.symbol)
-    end
-
-    private
-
-    def initialize_client
-        #token credentials can be found in config/initializers/iex_client.rb
-        @client ||= IEX::Api::Client.new
-    end
+    include ApiAccessor
 end
