f9280cc7cea9371628ebb69a48d37c178348b412
VILLAMOR GO <villamorgo@gmail.com>
2022-03-21 00:27:20 +0800

    Added layout for all transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,9 @@ class TransactionsController < ApplicationController
 
 #add before_action :authenticate_admin
   def index
-    @transactions = Transaction.all
+    @all_transactions = Transaction.all.order(id: "DESC")
+    @todays_transactions = Transaction.where(created_at: Date.current.all_day).order(id: "DESC")
+    @yesterdays_transactions = Transaction.where(created_at: Date.current.prev_day.all_day).order(id: "DESC")
   end
 
   def show
