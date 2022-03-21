85fdf70b948f3aa8275eb15bdd94f21579c880c1
VILLAMOR GO <villamorgo@gmail.com>
2022-03-21 21:16:50 +0800

    Added admin verification in transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
+  before_action :verify_is_admin, only: [:index, :show]
 
-#add before_action :authenticate_admin
   def index
     @all_transactions = Transaction.all.order(id: "DESC")
     @todays_transactions = Transaction.where(created_at: Date.current.all_day).order(id: "DESC")
@@ -10,4 +10,15 @@ class TransactionsController < ApplicationController
   def show
     @transaction = Transaction.find(params[:id])
   end
+
+  private
+
+  def verify_is_admin
+    if current_user.admin?
+       return
+    else
+       redirect_to root_path, notice: "You must be an admin to perform this action."
+    end
+  end
+
 end
