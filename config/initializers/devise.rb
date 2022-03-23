a9f63f8b030c036a492eb978e8f0125cfc7e0e15
Germaine Mojica <germaine.mojica@gmail.com>
2022-03-23 18:03:21 +0800

    Change mailer sender in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Stock App do-not-reply@gmail.com'
+  config.mailer_sender = 'vnggrpstockapp@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
