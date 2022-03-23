fb9068684f84291039f9c33d1bb54a33fb2b3258
Germaine Mojica <germaine.mojica@gmail.com>
2022-03-23 23:46:54 +0800

    Edit UserMailer model in pending request controller

--- app/controllers/pending_requests_controller.rb
+++ app/controllers/pending_requests_controller.rb
@@ -11,7 +11,7 @@ class PendingRequestsController < ApplicationController
     def update
         @user = User.find(params[:id])
         if @user.update(state: params[:state])
-          UserMailer.with(user: @user).approved_email.deliver_now
+          UserMailer.with(user: @user).approve_email.deliver_now
           redirect_to pending_requests_path, notice: "#{@user.email} has been #{@user.state}"
         end
     end
