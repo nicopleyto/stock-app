f9ac0e7997137e67f1955136f0c6fd2071327ec5
nicopleyto <nicopleyto@gmail.com>
2022-03-24 13:45:04 +0800

    added validations to total_quantity, removed comments

--- app/models/portfolio_stock.rb
+++ app/models/portfolio_stock.rb
@@ -1,15 +1,13 @@
 class PortfolioStock < ApplicationRecord
     belongs_to :user
-
+    validates :total_quantity, numericality {greater_than_or_equal_to: 0}
     include ApiAccessor
 
-    #self.access_quote.latest_price returns nilclass, must refactor
     def total_value
         self.access_quote.latest_price * self.total_quantity
     end
 
     def purge_zero_quantity_stock
-        #add code for deleting record if record has 0 quantity
         if self.total_quantity = 0
             self.destroy
         end
