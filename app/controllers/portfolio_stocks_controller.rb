8b45ef3cf352cf756a45a107f920e4337f56f37b
nicopleyto <nicopleyto@gmail.com>
2022-03-24 20:19:30 +0800

    added create transactions method for buy and selling stocks

--- app/controllers/portfolio_stocks_controller.rb
+++ app/controllers/portfolio_stocks_controller.rb
@@ -9,7 +9,7 @@ class PortfolioStocksController < ApplicationController
     @portfolio_stock = current_user.portfolio_stocks.find(params[:id])
   end
 
-  def buy #similar to edit action, set resource to be updated here (buy stock)
+  def buy
     stocksymbol = params[:stocksymbol]
     @portfolio_stock = current_user.portfolio_stocks.find_or_initialize_by(:symbol => stocksymbol) do |p|
       p.total_quantity = 0
@@ -17,7 +17,7 @@ class PortfolioStocksController < ApplicationController
     
   end
 
-  def update #similar to update action, add incremented quantity here (confirm purchase)
+  def update
     stocksymbol = portfolio_stock_params[:symbol]
     @portfolio_stock = current_user.portfolio_stocks.find_by(:symbol => stocksymbol)
 
@@ -25,17 +25,19 @@ class PortfolioStocksController < ApplicationController
     
     if @portfolio_stock.save
       redirect_to @portfolio_stock, notice: 'Stock was successfully bought.'
+      create_transaction_record(true)
     else
       render :buy
     end
 
   end
 
-  def create #similar to update action, add incremented quantity here (confirm purchase)
+  def create
     @portfolio_stock = current_user.portfolio_stocks.build(portfolio_stock_params)
 
     if @portfolio_stock.save
       redirect_to @portfolio_stock, notice: 'Stock was successfully bought.'
+      create_transaction_record(true)
     else
       render :buy
     end
@@ -55,7 +57,7 @@ class PortfolioStocksController < ApplicationController
 
     if @portfolio_stock.save
       redirect_to portfolio_stocks_path, notice: 'Stock was successfully sold.'
-      #create sell transaction
+      create_transaction_record(false)
       @portfolio_stock.purge_zero_quantity_stock
     else
       render :sell
@@ -67,4 +69,15 @@ class PortfolioStocksController < ApplicationController
   def portfolio_stock_params
     params.require(:portfolio_stock).permit(:symbol, :total_quantity)
   end
+
+  def create_transaction_record(transaction_type)
+    current_user.transactions.create(
+      :stock_symbol => @portfolio_stock.symbol,
+      :transaction_type_buy? => transaction_type,
+      :order_quantity => portfolio_stock_params[:total_quantity].to_d,
+      :price_during_transaction => @portfolio_stock.access_quote.latest_price,
+      :total_stock_amount => portfolio_stock_params[:total_quantity].to_d * @portfolio_stock.access_quote.latest_price
+    )
+  end
+
 end
