7b537b4d6454f03e34695b01a2ca0d58a86bfb35
VILLAMOR GO <villamorgo@gmail.com>
2022-03-26 09:37:58 +0800

    Restrict pending trader for buy and sell action

--- app/controllers/current_user_transactions_controller.rb
+++ app/controllers/current_user_transactions_controller.rb
@@ -1,9 +1,21 @@
 class CurrentUserTransactionsController < ApplicationController
+  before_action :approved_trader, only: [:index, :show]
+
   def index
-    @transactions = current_user.transactions.order(id: "DESC")
+    @transactions = current_user.transactions
   end
 
   def show
     @transaction = current_user.transactions.find(params[:id])
+  end
+
+  private
+
+  def approved_trader
+    if current_user.where(role: "trader", state: "Approved")
+       return
+    else
+       redirect_to root_path, notice: "Please wait until your application has been approved before doing this action."
+    end
   end
 end
