fe5b9e9d8ff5e04f7e48113fd4d73370886e5e56
nicopleyto <nicopleyto@gmail.com>
2022-03-27 18:06:39 +0800

    added balance column to user with default value of 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_054216) do
+ActiveRecord::Schema.define(version: 2022_03_27_093751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_03_22_054216) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "state", default: "Pending", null: false
+    t.decimal "balance", default: "0.0"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
