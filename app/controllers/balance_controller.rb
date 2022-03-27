7adba0d85c07c6c65495bc1d936a26327e8e381a
nicopleyto <nicopleyto@gmail.com>
2022-03-27 19:49:36 +0800

    added balance actions to controller

--- app/controllers/balance_controller.rb
+++ app/controllers/balance_controller.rb
@@ -1,14 +1,35 @@
 class BalanceController < ApplicationController
-  def show
+  def topup
     @balance = current_user.balance
   end
 
-  def topup
-  end
-
   def withdraw
+    @balance = current_user.balance
   end
 
-  def update
+  def confirm_topup
+    @balance += balance_params[:balance].to_d
+
+    if @balance.save
+      redirect_to root_path, notice: 'Top-up successful.'
+    else
+      render :topup
+    end
+  end
+
+  def confirm_withdraw
+    @balance -= balance_params[:balance].to_d
+
+    if @balance.save
+      redirect_to root_path, notice: 'Withdrawal successful.'
+    else
+      render :withdraw
+    end
+  end
+
+  private
+
+  def balance_params
+    params.require(:user).permit(:balance)
   end
 end
