c36e27507b3c1334cabf799a8c2c84fd7df11e20
nicopleyto <nicopleyto@gmail.com>
2022-03-27 18:03:01 +0800

    added request rspec tests

---
+++ spec/requests/balance_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Balances", type: :request do
+  describe "GET /show" do
+    it "returns http success" do
+      get "/balance/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
